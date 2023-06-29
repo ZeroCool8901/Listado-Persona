1c6a48d826a054feed948c4c3c9f76b2764d4549
Juan <juanrodriguez8901@hotmail.com>
2023-06-28 21:13:04 -0500

    Uso de injeccion de servicios

--- src/app/personasServices.ts
+++ src/app/personasServices.ts
@@ -1,12 +1,20 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { LoggingService } from "./LogginServices.service";
 import { Persona } from "./persona.model";
 
+@Injectable()
 export class PersonasService{
     personas: Persona[] = [
         new Persona('Juan', 'Perez'), 
         new Persona('Martin', 'Rodriguez')
     ];
 
+    saludar = new EventEmitter<number>();   
+
+    constructor(private loggingService: LoggingService){}
+
     agregarPersona(persona: Persona){
+        this.loggingService.enviaMensajeAConsola("agregamos persona: " + persona.nombre)
         this.personas.push(persona);
     }
 } 
